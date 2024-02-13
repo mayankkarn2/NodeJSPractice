2f416a4e985521c903e6a602ac99461cde384097
Karan <Mayank.Karan@commscope.com>
2024-02-13 16:02:22 +0530

    Added one more Middleware 2

--- sampleServer/app.js
+++ sampleServer/app.js
@@ -3,6 +3,12 @@ const express = require('express');
 const app = express();
 
 app.use(function (req, res, next) {
+    console.log('In Middleware 1')
+    next();
+})
+
+app.use(function (req, res, next) {
+    console.log('In Middleware 2')
     next();
 })
 
